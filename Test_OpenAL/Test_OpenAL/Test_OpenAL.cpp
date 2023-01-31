15635e70319dfeea559047f063739eeb9167ef31
DaTA677 <78419826+DaTA677@users.noreply.github.com>
2023-01-31 15:05:06 +0000

    Added OpenAL libraries and implemented Sound

---
+++ Test_OpenAL/Test_OpenAL/Test_OpenAL.cpp
@@ -0,0 +1,85 @@
+// Test_OpenAL.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include<AL/al.h>
+#include<AL/alc.h>
+#include <string>
+#include "Sound.h"
+
+
+#define OpenAL_ErrorCheck(message)\
+{\
+	ALenum error = alGetError(); \
+	if (error != AL_NO_ERROR)\
+	{\
+	std::cerr << "OpenAL Error:" << error << " with call for " << #message << std::endl;\
+		}\
+}
+
+#define alec(FUNCTION_CALL)\
+FUNCTION_CALL;\
+OpenAL_ErrorCheck(FUNCTION_CALL)
+
+int main()
+{
+
+	const ALCchar* defaultDeviceString = alcGetString(/*device*/ nullptr, ALC_DEFAULT_ALL_DEVICES_SPECIFIER);
+	ALCdevice* device = alcOpenDevice(defaultDeviceString);
+	if (!device) {
+		std::cerr << "Failed to get the Default device for OpenAL" << std::endl;
+	}
+	std::cout << "OpenAL Device: " << alcGetString(device, ALC_DEVICE_SPECIFIER) << std::endl;
+
+	ALCcontext* context = alcCreateContext(device, nullptr);
+	if (!alcMakeContextCurrent(context)) {
+		std::cerr << "Failed to make the OpenAL context the curent context" << std::endl;
+	}
+
+	alec(alListener3f(AL_POSITION, 0.0f, 0.0f, 0.0f));
+	alec(alListener3f(AL_VELOCITY, 0.0f, 0.0f, 0.0f));
+	ALfloat forwardAndUpVectors[] = {
+		/*forward = */ 1.0f,0.0f,0.0f,
+		/*up = */ 0.0f,1.0f,0.0f
+	};
+
+	alec(alListenerfv(AL_ORIENTATION, forwardAndUpVectors));
+
+	std::string name = "";
+	std::string extension = name.substr(name.length() - 3, 3);
+	ALuint buffer = 0;
+	if (extension == "wav") {
+		Sound* s = new Sound();
+		s->LoadFromWAV(name);
+		alGenBuffers(1, &buffer);
+		alBufferData(buffer, s->GetOALFormat(), s->GetData(), s->GetSize(), (ALsizei)s->GetFrequency());
+	}
+
+	ALuint monoSource;
+
+	alec(alGenSources(1, &monoSource));
+	alec(alSource3f(monoSource, AL_POSITION, 1.0f, 0.0f, 0.0f));
+	alec(alSource3f(monoSource, AL_VELOCITY, 0.0f, 0.0f, 0.0f));
+	alec(alSourcef(monoSource, AL_PITCH, 1.0f));
+	alec(alSourcef(monoSource, AL_GAIN, 1.0f));
+	alec(alSourcei(monoSource, AL_LOOPING, AL_FALSE));
+	alec(alSourcei(monoSource, AL_BUFFER, buffer));
+
+
+
+	std::cout<< "Hello World!\n";
+	alcMakeContextCurrent(nullptr);
+	alcDestroyContext(context);
+	alcCloseDevice(device);
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
